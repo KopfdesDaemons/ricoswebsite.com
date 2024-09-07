0683a2290ac4057190f651a68d25c4c9914a739e
Rico <mailvonrico@gmail.com>
2024-09-08 01:39:28 +0200

    fix thumbnail headline spelling, yaml loader with yaml parser

--- src/app/utilities/yamlLanguageLoader.ts
+++ src/app/utilities/yamlLanguageLoader.ts
@@ -1,41 +1,17 @@
 import { TranslateLoader } from '@ngx-translate/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { parse } from 'yaml';
 
 export class YamlLanguageLoader implements TranslateLoader {
-    constructor(private http: HttpClient) { }
+    constructor(
+        private http: HttpClient,
+        public path: string = '/assets/i18n/'
+    ) { }
 
-    getTranslation(lang: string): Observable<any> {
-        const path = `assets/i18n/${lang}.yaml`;
-
-        return new Observable(observer => {
-            this.http.get(path, { responseType: 'text' }).subscribe(
-                response => {
-                    const translations = this.parseYaml(response);
-                    observer.next(translations);
-                    observer.complete();
-                },
-                error => {
-                    console.error('Fehler beim Laden der Übersetzungen', error);
-                    observer.error(error);
-                }
-            );
-        });
-    }
-
-    private parseYaml(yamlString: string): { [key: string]: string } {
-        const lines = yamlString.split('\n');
-        const result: { [key: string]: string } = {};
-
-        lines.forEach(line => {
-            const delimiter = line.indexOf(':');
-            if (delimiter !== -1) {
-                const key = line.slice(0, delimiter).trim();
-                const value = line.slice(delimiter + 1).trim();
-                result[key] = value;
-            }
-        });
-
-        return result;
+    public getTranslation(lang: string): Observable<Object> {
+        return this.http
+            .get(`${this.path}${lang}.yaml`, { responseType: 'text' })
+            .pipe(map((data) => parse(data)));
     }
 }
