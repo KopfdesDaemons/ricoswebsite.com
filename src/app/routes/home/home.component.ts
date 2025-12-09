88148b0d5e416f64c41149705fb860489d9fb048
Rico <mailvonrico@gmail.com>
2025-12-10 00:01:56 +0100

    signal based project loading

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit, ElementRef, inject, OnDestroy, viewChild, signal, ChangeDetectionStrategy, afterRenderEffect } from '@angular/core';
+import { Component, ElementRef, inject, viewChild, signal, ChangeDetectionStrategy, afterRenderEffect, effect } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ProjectService } from 'src/app/services/project.service';
 import { Project } from 'src/app/models/project.model';
 import { LanguageService } from 'src/app/services/language.service';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
-import { Subscription, lastValueFrom } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
 import { ProjectCardComponent } from '../../components/project-card/project-card.component';
 import { FormsModule } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home',
@@ -16,7 +17,7 @@ import { FormsModule } from '@angular/forms';
   imports: [ProjectCardComponent, RouterLink, TranslateModule, FormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent {
   protected languageS = inject(LanguageService);
   private meta = inject(Meta);
   private title = inject(Title);
@@ -25,47 +26,37 @@ export class HomeComponent implements OnInit, OnDestroy {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
 
+  private routeParams = toSignal(this.route.params);
+  private queryParams = toSignal(this.route.queryParams);
+
   protected projects = signal<Project[]>([]);
   protected technologiesFilterOptions: string[] = [];
   protected activeTechnologiesFilter: string[] = [];
   protected searchQuery = '';
   protected totalPages = signal<number>(0);
   protected currentPage = signal<number>(1);
-  protected currentLang = signal<string>('en');
   private totalProjects: number = 0;
   private projectsPerPage = 5;
   readonly projectsSection = viewChild.required<ElementRef>('projectsSection');
   private searchInput = viewChild<ElementRef>('searchInput');
-  private routeParamsSubscription: Subscription | undefined;
-  private queryParamsSubscription: Subscription | undefined;
 
   constructor() {
+    effect(async () => {
+      // Re-load projects when language, route params or query params change
+      this.languageS.userLanguage();
+      this.routeParams();
+      this.queryParams();
+
+      await this.loadProjects();
+    });
+
     afterRenderEffect(() => {
       if (this.projects().length > 0 && this.route.snapshot.fragment === 'projectsSection') {
         this.projectsSection().nativeElement?.scrollIntoView({ behavior: 'smooth' });
       }
-      if (this.searchQuery) {
+      if (this.queryParams()?.['search']) {
         this.searchInput()?.nativeElement.focus();
       }
-    });
-  }
-
-  async ngOnInit() {
-    await this.loadProjects();
-
-    this.routeParamsSubscription = this.route.params.subscribe(async (params) => {
-      const { page, lang } = params;
-      if (this.currentPage() === +page && this.currentLang() === lang) return;
-      this.currentLang.set(lang);
-      this.currentPage.set(+page || 1);
-      await this.loadProjects();
-    });
-
-    this.queryParamsSubscription = this.route.queryParams.subscribe(async (params) => {
-      const { technologies, search } = params;
-      this.activeTechnologiesFilter = technologies ? technologies.split('&') : [];
-      this.searchQuery = search || '';
-      await this.loadProjects();
     });
   }
 
@@ -78,16 +69,21 @@ export class HomeComponent implements OnInit, OnDestroy {
     ]);
   }
 
-  ngOnDestroy(): void {
-    this.routeParamsSubscription?.unsubscribe();
-    this.queryParamsSubscription?.unsubscribe();
-  }
-
   async loadProjects() {
-    await this.setMetaTags();
-    const filter = this.activeTechnologiesFilter;
+    console.log('load projects');
 
-    const [projects, allTechnologies] = await Promise.all([this.ps.getProjects(filter, this.projectsPerPage, this.currentPage(), this.searchQuery), this.ps.getAllTechnologies()]);
+    const params = this.routeParams();
+    const queryParams = this.queryParams();
+    if (!params || !queryParams) return;
+
+    await this.setMetaTags();
+
+    const page = +(params['page'] || 1);
+    this.currentPage.set(page);
+    this.activeTechnologiesFilter = queryParams['technologies'] ? queryParams['technologies'].split('&') : [];
+    this.searchQuery = queryParams['search'] || '';
+
+    const [projects, allTechnologies] = await Promise.all([this.ps.getProjects(this.activeTechnologiesFilter, this.projectsPerPage, page, this.searchQuery), this.ps.getAllTechnologies()]);
 
     this.projects.set(projects.projects);
     this.totalProjects = projects.total;
